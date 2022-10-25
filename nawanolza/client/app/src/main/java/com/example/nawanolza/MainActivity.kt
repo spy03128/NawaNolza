d7df48ba5cc7994b10b9082ff1049b8c23caf5d7
nogusdn <97591002+nogusdn@users.noreply.github.com>
2022-10-25 13:40:12 +0900

    S07P31D103-261 홈화면네비게이션기능

--- nawanolza/client/app/src/main/java/com/example/nawanolza/MainActivity.kt
+++ nawanolza/client/app/src/main/java/com/example/nawanolza/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.nawanolza
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.example.nawanolza.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = nav_host_fragment.findNavController()
     }
 }

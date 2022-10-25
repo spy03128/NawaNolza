fb8e4f006283ca75f80e6ba121d93db350cff817
workcom0 <90753448+workcom0@users.noreply.github.com>
2022-10-25 14:26:11 +0900

    S07P31D103-148 테이블 수정

--- nawanolza/db/nawanolza.sql
+++ nawanolza/db/nawanolza.sql
@@ -69,10 +69,10 @@ DROP TABLE IF EXISTS `character_type`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `character_type` (
-  `history_id` bigint NOT NULL AUTO_INCREMENT,
+  `character_type_id` bigint NOT NULL AUTO_INCREMENT,
   `character_id` bigint NOT NULL,
   `type_id` bigint NOT NULL,
-  PRIMARY KEY (`history_id`),
+  PRIMARY KEY (`character_type_id`),
   CONSTRAINT `histories_fk_characters` FOREIGN KEY (`character_id`) REFERENCES `characters` (`character_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `histories_fk_types` FOREIGN KEY (`type_id`) REFERENCES `types` (`type_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;

dffcc94809b76d95277f962d3a620fc67f35dc59
권도현 <tobeykwon@gmail.com>
2022-10-27 02:34:02 +0900

    S07P31D103-282 HideAndGameRoomService createGameRoom 메서드 방생성 제한 로직 추가

--- nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/service/HideAndGameRoomService.java
+++ nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/service/HideAndGameRoomService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ssafy.nawanolza.server.domain.entity.dto.GameRoom;
 import ssafy.nawanolza.server.domain.entity.dto.HideAndSeekGameRoom;
+import ssafy.nawanolza.server.domain.exception.CreateGameRoomLimitException;
 import ssafy.nawanolza.server.domain.exception.GameRoomNotFoundException;
 import ssafy.nawanolza.server.domain.repository.GameRoomRepository;
 import ssafy.nawanolza.server.domain.utils.CreateRoomUtil;
@@ -16,6 +17,8 @@ public class HideAndGameRoomService {
     private final GameRoomRepository gameRoomRepository;
 
     public HideAndSeekGameRoom createGameRoom(Long hostId) {
+        if (!gameRoomRepository.isCreatableGameRoom())
+            throw new CreateGameRoomLimitException();
         HideAndSeekGameRoom createGameRoom = HideAndSeekGameRoom.create(hostId, createRoomUtill);
         return (HideAndSeekGameRoom) gameRoomRepository.save(createGameRoom);
     }

add84525efec57c20ab58818bc3d0bdc128930b7
권도현 <tobeykwon@gmail.com>
2022-10-27 09:21:13 +0900

    S07P31D103-282 GameRoomNotFoundException 오타 수정

--- nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/exception/GameRoomNotFoundException.java
+++ nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/exception/GameRoomNotFoundException.java
@@ -3,6 +3,6 @@ package ssafy.nawanolza.server.domain.exception;
 public class GameRoomNotFoundException extends IllegalArgumentException {
 
     public GameRoomNotFoundException() {
-        super("요청하시 게임방 정보가 존재하지 않습니다.");
+        super("요청하신 게임방 정보가 존재하지 않습니다.");
     }
 }

3f337ace30ac86617d6f0fbb216958d95f5cddca
workcom0 <90753448+workcom0@users.noreply.github.com>
2022-10-27 22:18:28 +0900

    S07P31D103-283 지도에 캐릭터 위치 조회 서비스 로직 추가

--- nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/service/CollectionService.java
+++ nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/service/CollectionService.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ssafy.nawanolza.server.domain.entity.Collection;
 import ssafy.nawanolza.server.domain.entity.Member;
+import ssafy.nawanolza.server.domain.entity.dto.Marker;
 import ssafy.nawanolza.server.domain.exception.MemberNotFountException;
 import ssafy.nawanolza.server.domain.repository.CollectionCustomRepositoryImpl;
+import ssafy.nawanolza.server.domain.repository.MapCharacterRedisRepository;
 import ssafy.nawanolza.server.domain.repository.MemberRepository;
 
 import java.util.List;
@@ -15,11 +17,16 @@ import java.util.List;
 public class CollectionService {
     private final MemberRepository memberRepository;
     private final CollectionCustomRepositoryImpl collectionCustomRepository;
+    private final MapCharacterRedisRepository mapCharacterRedisRepository;
 
     public List<Collection> getCollection(Long memberId, String type, String sort){
         Member findMember = memberRepository.findById(memberId)
                 .orElseThrow(() -> new MemberNotFountException(memberId));
 
         return collectionCustomRepository.getCollection(memberId, type, sort);
+    }
+
+    public List<Marker> getMapCharacters(){
+        return (List<Marker>) mapCharacterRedisRepository.findAll();
     }
 }

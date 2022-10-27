1166cb5eab99d7df3a449ead277c5cf1d8bc50ec
jihyeon <spy03128@naver.com>
2022-10-27 14:02:18 +0900

    S07P31D103-263 구글 지도 불러오기

--- nawanolza/client/app/src/main/java/com/example/nawanolza/fragment/CharacterHomeFragment.kt
+++ nawanolza/client/app/src/main/java/com/example/nawanolza/fragment/CharacterHomeFragment.kt
@@ -1,11 +1,15 @@
 package com.example.nawanolza.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.nawanolza.R
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.NaverMapSdk
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,6 +22,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CharacterHomeFragment : Fragment() {
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -28,7 +33,9 @@ class CharacterHomeFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-    }
+//        NaverMapSdk.getInstance(Context).client=
+//            NaverMapSdk.NaverCloudPlatformClient("6cd3v8foff")
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,6 +54,10 @@ class CharacterHomeFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment CharacterHomeFragment.
          */
+
+        lateinit var naverMap: NaverMap
+
+
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =

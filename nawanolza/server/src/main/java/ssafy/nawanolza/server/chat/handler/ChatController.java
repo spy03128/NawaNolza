6fcea992090ec75440f60396d1bf017fc07144db
sawol <55649302+sawol@users.noreply.github.com>
2022-10-27 09:46:39 +0900

    웹으로 채팅 되는 거 확인함

--- nawanolza/server/src/main/java/ssafy/nawanolza/server/chat/handler/ChatController.java
+++ nawanolza/server/src/main/java/ssafy/nawanolza/server/chat/handler/ChatController.java
@@ -1,16 +1,16 @@
 package ssafy.nawanolza.server.chat.handler;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/chat")
 public class ChatController {
-
     private final ChatService chatService;
 
     // 채팅 리스트 화면

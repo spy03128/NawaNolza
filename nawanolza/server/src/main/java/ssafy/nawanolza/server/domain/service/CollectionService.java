49a663ad851d2d129dbd84892f5384269cf0843e
workcom0 <90753448+workcom0@users.noreply.github.com>
2022-10-30 21:08:25 +0900

    S07P31D103-290 캐릭터 상세페이지 조회 서비스 추가

--- nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/service/CollectionService.java
+++ nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/service/CollectionService.java
@@ -1,18 +1,16 @@
 package ssafy.nawanolza.server.domain.service;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ssafy.nawanolza.server.domain.entity.*;
 import ssafy.nawanolza.server.domain.entity.Character;
-import ssafy.nawanolza.server.domain.entity.Collection;
-import ssafy.nawanolza.server.domain.entity.Game;
-import ssafy.nawanolza.server.domain.entity.Member;
 import ssafy.nawanolza.server.domain.entity.dto.Marker;
+import ssafy.nawanolza.server.domain.exception.CharacterNotFountException;
+import ssafy.nawanolza.server.domain.exception.CollectionNotFountException;
 import ssafy.nawanolza.server.domain.exception.MemberNotFountException;
 import ssafy.nawanolza.server.domain.repository.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -21,6 +19,10 @@ import java.util.Random;
 public class CollectionService {
     private final MemberRepository memberRepository;
     private final CollectionCustomRepositoryImpl collectionCustomRepository;
+    private final CollectionRepository collectionRepository;
+    private final TypeRepository typeRepository;
+    private final CharacterTypeRepository characterTypeRepository;
+    private final HistoryRepository historyRepository;
     private final MapCharacterRedisRepository mapCharacterRedisRepository;
     private final CharacterRepository characterRepository;
     private final GameRepository gameRepository;
@@ -36,6 +38,27 @@ public class CollectionService {
                 .orElseThrow(() -> new MemberNotFountException(memberId));
 
         return collectionCustomRepository.getCollection(memberId, type, sort);
+    }
+
+    public Character getCharacterDetail(Long characterId){
+        return characterRepository.findById(characterId)
+                .orElseThrow(() -> new CharacterNotFountException(characterId));
+    }
+
+    public Collection getCollectionDetail(Long memberId, Long characterId){
+        Member findMember = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberNotFountException(memberId));
+
+        return collectionRepository.findByMemberIdAndCharacterId(findMember.getId(), characterId)
+                .orElseThrow(() -> new CollectionNotFountException(memberId,characterId));
+    }
+
+    public List<String> getTypesDetail(Character character){
+        return typeRepository.findAllByType(characterTypeRepository.findAllByCharacter(character));
+    }
+
+    public List<History> getHistoryDetail(Collection collection){
+        return historyRepository.findAllByCollection(collection);
     }
 
     public List<Marker> getMapCharacters(){

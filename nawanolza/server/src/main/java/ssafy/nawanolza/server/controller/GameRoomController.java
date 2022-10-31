53e22c962038e8ff4827707ebb1076a18f5ecf6c
권도현 <tobeykwon@gmail.com>
2022-10-31 10:14:44 +0900

    S07P31D103-284 숨바꼭질 gps 소켓 개발

---
+++ nawanolza/server/src/main/java/ssafy/nawanolza/server/controller/GameRoomController.java
@@ -0,0 +1,25 @@
+package ssafy.nawanolza.server.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+import ssafy.nawanolza.server.domain.socket.dto.GameRoomGpsDTO;
+
+@Controller
+@RequiredArgsConstructor
+public class GameRoomController {
+
+    private final SimpMessageSendingOperations simpMessageSendingOperations;
+
+    /*
+        /sub/gps/{uuid}    - 구독(roomId:UUID)
+        /pub/gps            - GPS 정보 전달
+     */
+
+    @MessageMapping("/gps")
+    public void gpsSend(GameRoomGpsDTO gameRoomGpsDTO) {
+        simpMessageSendingOperations.convertAndSend("/sub/gps/" + gameRoomGpsDTO.getGameRoomId() , gameRoomGpsDTO);
+    }
+
+}

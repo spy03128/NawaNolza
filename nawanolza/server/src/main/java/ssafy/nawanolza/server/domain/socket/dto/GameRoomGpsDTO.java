b2bbdb9f7e0e7c9e55fefc7d89d2e3dfc9bbb7f1
권도현 <tobeykwon@gmail.com>
2022-10-31 10:14:44 +0900

    S07P31D103-284 숨바꼭질 gps 소켓 개발

--- nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/socket/dto/GameRoomGpsDTO.java
+++ nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/socket/dto/GameRoomGpsDTO.java
@@ -1,15 +1,21 @@
 package ssafy.nawanolza.server.domain.socket.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * lat: 위도
  * lng: 경도
  * memberId: 멤버 Id
  */
-@Data
-public class GameRoomGpsDTO {
+@Getter
+@Setter
+public class GameRoomGpsDTO extends GameRoomBaseDTO {
     private Double lat;
     private Double lng;
-    private Long memberId;
+
+    public GameRoomGpsDTO() {
+        super(Type.GPS);
+    }
 }

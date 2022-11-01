1bd8b4f1cd2320088869caed400e8b48afa9b182
권도현 <tobeykwon@gmail.com>
2022-11-01 17:42:25 +0900

    S07P31D103-384 GameRoomController 소켓 및 api 추가

--- nawanolza/server/src/main/java/ssafy/nawanolza/server/controller/GameRoomController.java
+++ nawanolza/server/src/main/java/ssafy/nawanolza/server/controller/GameRoomController.java
@@ -1,17 +1,33 @@
 package ssafy.nawanolza.server.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ssafy.nawanolza.server.domain.entity.dto.HideAndSeekGameRoom;
+import ssafy.nawanolza.server.domain.repository.MemberRepository;
+import ssafy.nawanolza.server.domain.service.HideAndGameRoomService;
 import ssafy.nawanolza.server.domain.socket.dto.GameEventDTO;
 import ssafy.nawanolza.server.domain.socket.dto.GameRoomGpsDTO;
+import ssafy.nawanolza.server.dto.CreateGameRequest;
+import ssafy.nawanolza.server.dto.CreateGameRoomResponse;
+import ssafy.nawanolza.server.dto.EntryGameRoomResponse;
+import ssafy.nawanolza.server.dto.MemberDto;
 
-@Controller
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
 @RequiredArgsConstructor
 public class GameRoomController {
 
     private final SimpMessageSendingOperations simpMessageSendingOperations;
+    private final HideAndGameRoomService hideAndGameRoomService;
+    private final MemberRepository memberRepository;
 
     /*
         /sub/gps/{uuid}    - 구독(roomId:UUID)
@@ -26,6 +42,24 @@ public class GameRoomController {
     @MessageMapping("/event")
     public void eventBroadCast(GameEventDTO gameEventDTO) {
         simpMessageSendingOperations.convertAndSend("/sub/event" + gameEventDTO.getGameRoomId(), gameEventDTO);
+    }
+
+    @PostMapping("/hideandseek")
+    public ResponseEntity<CreateGameRoomResponse> createRoomResponse(@RequestBody CreateGameRequest request) {
+        HideAndSeekGameRoom gameRoom = hideAndGameRoomService.createGameRoom(request.getHostId(), request.makeHideAndSeekProperties());
+        return ResponseEntity.ok().body(new CreateGameRoomResponse(gameRoom));
+    }
+
+    @PostMapping("/hideandseek/participation")
+    public ResponseEntity<EntryGameRoomResponse> participateHideAndSeek(@RequestBody Map<String, String> request) {
+        long memberId = Long.parseLong(request.get("memberId"));
+        String entryCode = request.get("entryCode");
+
+        // 게임 참여
+        HideAndSeekGameRoom hideAndSeekGameRoom = hideAndGameRoomService.paticipateGameRoom(memberId, entryCode);
+        List<MemberDto> members = memberRepository.findAllByMemberId(hideAndSeekGameRoom.getParticipants()).stream().map(MemberDto::new).collect(Collectors.toList());
+        simpMessageSendingOperations.convertAndSend("/sub/participate" + entryCode, members);
+        return ResponseEntity.ok().body(new EntryGameRoomResponse(hideAndSeekGameRoom, members));
     }
 
 }

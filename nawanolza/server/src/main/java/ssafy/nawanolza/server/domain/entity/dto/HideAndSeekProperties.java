d09a217a3092de58b36a790ba3d4eff434893438
권도현 <tobeykwon@gmail.com>
2022-11-01 17:39:41 +0900

    S07P31D103-384 HideAndSeekProperties 추가

---
+++ nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/entity/dto/HideAndSeekProperties.java
@@ -0,0 +1,14 @@
+package ssafy.nawanolza.server.domain.entity.dto;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor @Builder
+public class HideAndSeekProperties {
+    private double lat;
+    private double lng;
+    private long playTime;
+    private long hideTime;
+    private int range;
+}

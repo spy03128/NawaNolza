7ec05126d5a7c6501ee56fd1caee199a200796f4
sawol <55649302+sawol@users.noreply.github.com>
2022-11-04 12:09:13 +0900

    S07P31D103-287 바이트 변환 메서드 삭제

--- nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/service/MarkerService.java
+++ nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/service/MarkerService.java
@@ -152,17 +152,4 @@ public class MarkerService {
         double lat;
         double lng;
     }
-
-    public static byte[] longToByteArray(long data) {
-        return new byte[] {
-            (byte)((data >> 56) & 0xff),
-            (byte)((data >> 48) & 0xff),
-            (byte)((data >> 40) & 0xff),
-            (byte)((data >> 32) & 0xff),
-            (byte)((data >> 24) & 0xff),
-            (byte)((data >> 16) & 0xff),
-            (byte)((data >> 8 ) & 0xff),
-            (byte)((data >> 0) & 0xff),
-        };
-    }
 }

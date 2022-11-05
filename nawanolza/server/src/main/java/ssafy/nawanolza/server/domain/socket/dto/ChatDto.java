b53ef8f633e795f0702f2caed90e74727301867f
sawol <sawol634@gmail.com>
2022-11-05 12:56:54 +0900

    S07P31D103-409 Dto 상속으로 변경

--- nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/socket/dto/ChatDto.java
+++ nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/socket/dto/ChatDto.java
@@ -2,25 +2,20 @@ package ssafy.nawanolza.server.domain.socket.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
-public class ChatDto {
-
-    @NotNull
-    private Long senderId;
+public class ChatDto extends GameRoomBaseDTO{
 
     @NotNull
     private String senderImage;
 
-    @NotNull
-    private Long roomId;
-
     @NotBlank
     private String message;
+
+    public ChatDto() {
+        super(Type.CHAT);
+    }
 }

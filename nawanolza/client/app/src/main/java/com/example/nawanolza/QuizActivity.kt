9c1f7ec151efa490e70889ac7618e097575ce38e
jihyeon <spy03128@naver.com>
2022-11-10 14:18:56 +0900

    S07P31D103-392 퀴즈 화면 디자인 변경

--- nawanolza/client/app/src/main/java/com/example/nawanolza/QuizActivity.kt
+++ nawanolza/client/app/src/main/java/com/example/nawanolza/QuizActivity.kt
@@ -1,10 +1,15 @@
 package com.example.nawanolza
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.example.nawanolza.databinding.ActivityQuizBinding
 import com.example.nawanolza.retrofit.*
+import com.google.gson.GsonBuilder
+import kotlinx.android.synthetic.main.activity_card_game.*
 import kotlinx.android.synthetic.main.activity_quiz.*
+import kotlinx.android.synthetic.main.activity_quiz.timer
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -25,8 +30,10 @@ class QuizActivity : AppCompatActivity() {
         val markerId = intent.getLongExtra("markerId",-1)
         val memberId = LoginUtil.getMember(this)!!.id
         val characterId = intent.getLongExtra("characterId",-1)
+        val quizInfo : QuestResponse = GsonBuilder().create().fromJson(intent.getStringExtra("quizInfo"), QuestResponse::class.java)
 
-
+        println("===============quizactivity quizinfo")
+        println(quizInfo)
 
         //퀴즈 받아오기
         var retrofit = Retrofit.Builder()
@@ -35,40 +42,58 @@ class QuizActivity : AppCompatActivity() {
             .build()
 
         var service = retrofit.create(QuestService::class.java)
+//
+        binding.title.text = quizInfo.quiz.context
 
 
+        object : CountDownTimer(10000, 1000) {
 
-        service.GetQuiz(mapOf("markerId" to markerId.toString(), "questType" to "0")).enqueue(object: Callback<QuestResponse> {
+            override fun onTick(millisUntilFinished: Long) {
+                var time = (millisUntilFinished / 1000).toInt()
+                timer.text = time.toString()
 
-            override fun onResponse(
-                call: Call<QuestResponse>,
-                response: Response<QuestResponse>
-            ) {
-                val body = response.body()
-
-                quizInfo = response.body() ?: QuestResponse()
-
-
-                println(quizInfo)
-                println("=====quiz!!!!======")
-
-                binding.title.text = body?.quiz?.context
             }
 
-            override fun onFailure(call: Call<QuestResponse>, t: Throwable) {
-                println(call)
-                println(t)
-                println("====quiz 에러 ===")
-            }
+            override fun onFinish() {
+                QuestUtil.quizFail(this@QuizActivity, service, markerId)
 
-        })
+                val intent = Intent(this@QuizActivity, MapActivity::class.java)
+//                    intent.putExtra("result",false)
+                setResult(RESULT_CANCELED, intent)
+                finish()
+            }
+        }.start()
+//
+//        service.GetQuiz(mapOf("markerId" to markerId.toString(), "questType" to "0")).enqueue(object: Callback<QuestResponse> {
+//
+//            override fun onResponse(
+//                call: Call<QuestResponse>,
+//                response: Response<QuestResponse>
+//            ) {
+//                val body = response.body()
+//
+//                quizInfo = response.body() ?: QuestResponse()
+//
+//
+//                println(quizInfo)
+//                println("=====quiz!!!!======")
+//
+//                binding.title.text = body?.quiz?.context
+//            }
+//
+//            override fun onFailure(call: Call<QuestResponse>, t: Throwable) {
+//                println(call)
+//                println(t)
+//                println("====quiz 에러 ===")
+//            }
+//
+//        })
 
 
 
         exitButton.setOnClickListener{
             finish()
         }
-
 
         Obutton.setOnClickListener{
             if(quizInfo.quiz.answer==true){

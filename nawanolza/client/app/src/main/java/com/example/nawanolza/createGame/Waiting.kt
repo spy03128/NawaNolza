61c3e08c8194cd634c8e64f095b605b0ee038c28
정동규 <1217jdk@naver.com>
2022-11-10 15:37:02 +0900

    S07P31D103-531 숨바꼭질 게임 시작

--- nawanolza/client/app/src/main/java/com/example/nawanolza/createGame/Waiting.kt
+++ nawanolza/client/app/src/main/java/com/example/nawanolza/createGame/Waiting.kt
@@ -4,17 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import com.bumptech.glide.Glide
 import com.example.nawanolza.databinding.ActivityWaitingBinding
 import com.example.nawanolza.hideandseek.RoleCheckActivity
+import com.example.nawanolza.retrofit.RetrofitConnection
 import com.example.nawanolza.retrofit.createroom.MemberList
 import com.example.nawanolza.retrofit.enterroom.EnterRoomResponse
+import com.example.nawanolza.retrofit.enterroom.GetRoomResponse
+import com.example.nawanolza.retrofit.enterroom.GetRoomService
 import com.example.nawanolza.stomp.SocketType
 import com.example.nawanolza.stomp.waitingstomp.WaitingStompClient
 import com.google.gson.GsonBuilder
 import io.reactivex.Emitter
 import kotlinx.android.synthetic.main.activity_waiting.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.create
 import ua.naiksoftware.stomp.Stomp
 import ua.naiksoftware.stomp.dto.LifecycleEvent
 import kotlin.concurrent.timer
@@ -53,11 +61,27 @@ class Waiting : AppCompatActivity() {
         hostName.text = roomInfo.host.name
         Glide.with(this).load(roomInfo.host.image).circleCrop().into(hostImg)
 
-        binding.btnStart.setOnClickListener {
-            val intent = Intent(this, RoleCheckActivity::class.java)
-            intent.putExtra("entryCode", entryCode)
+        var retrofitAPI = RetrofitConnection.getInstance().create(
+            GetRoomService::class.java
+        )
 
-            startActivity(intent)
+        binding.btnStart.setOnClickListener {
+            retrofitAPI.getRoomInfo(entryCode.toString()).enqueue(object:
+                Callback<GetRoomResponse> {
+                override fun onResponse(
+                    call: Call<GetRoomResponse>,
+                    response: Response<GetRoomResponse>
+                ) {
+                    when(response.code()){
+                        else -> Toast.makeText(this@Waiting, "게임 시작 실패", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<GetRoomResponse>, t: Throwable) {
+                    println(call)
+                    println(t)
+                }
+            })
         }
         adapter = WaitingRvAdapter(this)
         binding.mRecyclerView.adapter = adapter
@@ -67,6 +91,7 @@ class Waiting : AppCompatActivity() {
 
         WaitingStompClient.connect()
         WaitingStompClient.receive(SocketType.PARTICIPATE, entryCode.toString(), adapter, this)
+        WaitingStompClient.subGameStart(entryCode.toString(), this@Waiting)
 
         check.setOnClickListener {
             check()

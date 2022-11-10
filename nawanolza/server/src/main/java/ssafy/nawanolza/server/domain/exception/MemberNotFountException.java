a678a7c7f5681bd2b7cd8b182ca4d6f715600bf9
sawol <sawol634@gmail.com>
2022-11-11 01:23:29 +0900

    S07P31D103-521 아이디로 멤버 조회 Optional로 변경

--- nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/exception/MemberNotFountException.java
+++ nawanolza/server/src/main/java/ssafy/nawanolza/server/domain/exception/MemberNotFountException.java
@@ -1,5 +1,7 @@
 package ssafy.nawanolza.server.domain.exception;
 
+import java.util.List;
+
 public class MemberNotFountException extends RuntimeException {
 
     private final Object data;
@@ -12,5 +14,10 @@ public class MemberNotFountException extends RuntimeException {
     public MemberNotFountException(Long memberId) {
         super("요청하신 회원정보가 존재하지 않습니다.");
         this.data = memberId;
+    }
+
+    public MemberNotFountException(List<Long> memberIds) {
+        super("요청하신 회원정보가 존재하지 않습니다.");
+        this.data = memberIds;
     }
 }

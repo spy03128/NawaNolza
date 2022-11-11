e48db365099042d2bc1f9f7e188bb998657457af
nogusdn <97591002+nogusdn@users.noreply.github.com>
2022-11-11 17:50:45 +0900

    S07P31D103-494 유저상태 리사이클러 뷰 설정

--- nawanolza/client/app/src/main/java/com/example/nawanolza/hideandseek/HideSeekRvAdapter.kt
+++ nawanolza/client/app/src/main/java/com/example/nawanolza/hideandseek/HideSeekRvAdapter.kt
@@ -1,12 +1,16 @@
 package com.example.nawanolza.hideandseek
 
+import android.app.Application
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.nawanolza.createGame.WaitingMember
+import com.bumptech.glide.Glide
+import com.example.nawanolza.MarkerImageUtil
+import com.example.nawanolza.createGame.Waiting
 import com.example.nawanolza.databinding.HideseekmemberRvItemBinding
 
-class HideSeekRvAdapter (private val waitingMember: ArrayList<WaitingMember>)
+class HideSeekRvAdapter (private val context: Context)
     :RecyclerView.Adapter<HideSeekRvAdapter.MyViewHolder>(){
         inner class MyViewHolder(binding: HideseekmemberRvItemBinding):
             RecyclerView.ViewHolder(binding.root){
@@ -22,11 +26,16 @@ class HideSeekRvAdapter (private val waitingMember: ArrayList<WaitingMember>)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val waitingData = waitingMember[position]
-        holder.username.text = waitingData.name
+        val waitingData = Waiting.memberList[position]
+
+        holder.apply{
+            Glide.with(context).load(waitingData.image).circleCrop().into(profileImg)
+            username.text = waitingData.name
+
+        }
     }
 
     override fun getItemCount(): Int {
-        return waitingMember.size
+        return Waiting.memberList.size
     }
-    }
+}

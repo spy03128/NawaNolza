c2562ce766d49dc90da6c9620d9109e62f308a1b
권도현 <tobeykwon@gmail.com>
2022-11-12 15:14:45 +0900

    S07P31D103-382 ChattingUtil 변경

--- nawanolza/client/app/src/main/java/com/example/nawanolza/hideandseek/ChattingUtil.kt
+++ nawanolza/client/app/src/main/java/com/example/nawanolza/hideandseek/ChattingUtil.kt
@@ -1,19 +1,24 @@
 package com.example.nawanolza.hideandseek
 
+import com.example.nawanolza.stomp.SocketChatDTO
+
 class ChattingUtil {
     companion object {
-        val chatMap: HashMap<String, ArrayList<ChatDto>> = HashMap()
+        val chatMap: HashMap<String, ArrayList<SocketChatDTO>> = HashMap()
 
         fun existChatData(entryCode: String) : Boolean {
             return chatMap.containsKey(entryCode)
         }
 
-        fun getChatData(entryCode: String): ArrayList<ChatDto> {
+        fun getChatData(entryCode: String): ArrayList<SocketChatDTO> {
+            if (!existChatData(entryCode))
+                chatMap.put(entryCode, ArrayList())
             return chatMap.get(entryCode)!!
         }
 
         fun clearChatData(entryCode: String) {
             chatMap.get(entryCode)!!.clear()
+            chatMap.remove(entryCode)
         }
     }
 }

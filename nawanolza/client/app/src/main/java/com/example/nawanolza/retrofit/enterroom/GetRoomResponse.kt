78c72c4071b1d9a4badca166c18e054593686f8f
nogusdn <97591002+nogusdn@users.noreply.github.com>
2022-11-12 14:15:57 +0900

    S07P31D103-424-참여자리스트 업데이트

--- nawanolza/client/app/src/main/java/com/example/nawanolza/retrofit/enterroom/GetRoomResponse.kt
+++ nawanolza/client/app/src/main/java/com/example/nawanolza/retrofit/enterroom/GetRoomResponse.kt
@@ -2,7 +2,7 @@ package com.example.nawanolza.retrofit.enterroom
 
 data class GetRoomResponse(
     val entryCode: String,
-    val tagger: Int,
+    val tagger: List<Int>,
     val runners: List<Int>,
     val startTime: String,
     val playTime: Long,

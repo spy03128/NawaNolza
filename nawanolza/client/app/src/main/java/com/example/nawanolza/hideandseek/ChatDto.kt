d05c31195bc29d161e3273751f6fcde0db230be7
권도현 <tobeykwon@gmail.com>
2022-11-12 15:13:29 +0900

    S07P31D103-382 ChatDto 삭제

--- nawanolza/client/app/src/main/java/com/example/nawanolza/hideandseek/ChatDto.kt
+++
@@ -1,9 +0,0 @@
-package com.example.nawanolza.hideandseek
-
-data class ChatDto(
-    val senderId: Long,
-    val senderImage: String,
-    val senderName: String,
-    val entryCode: String,
-    val message: String,
-)

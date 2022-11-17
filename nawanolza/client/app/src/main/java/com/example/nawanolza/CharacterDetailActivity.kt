d820b2ddb9c995422650b8f6fd9b39070c73ca96
jihyeon <spy03128@naver.com>
2022-11-17 12:45:06 +0900

    S07P31D103-642 캐릭터 상세페이지 정보 통신

--- nawanolza/client/app/src/main/java/com/example/nawanolza/CharacterDetailActivity.kt
+++ nawanolza/client/app/src/main/java/com/example/nawanolza/CharacterDetailActivity.kt
@@ -2,10 +2,67 @@ package com.example.nawanolza
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nawanolza.retrofit.CharacterDetailResponse
+import kotlinx.android.synthetic.main.activity_character.*
+import kotlinx.android.synthetic.main.activity_character_detail.*
+import kotlinx.android.synthetic.main.activity_character_detail.close
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class CharacterDetailActivity : AppCompatActivity() {
+
+    var characterDetailInfo = CharacterDetailResponse()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_character_detail)
+
+
+        val characterId = intent.getIntExtra("characterId", -1)
+        val memberId = LoginUtil.getMember(this)!!.id
+
+
+        var retrofit = Retrofit.Builder()
+            .baseUrl("https://k7d103.p.ssafy.io/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        var service = retrofit.create(CharacterDetailService::class.java)
+
+
+
+
+        service.GetCharacterDetail(memberId, characterId).enqueue(object: Callback<CharacterDetailResponse> {
+
+            override fun onResponse(
+                call: Call<CharacterDetailResponse>,
+                response: Response<CharacterDetailResponse>
+            ) {
+
+                characterDetailInfo = response.body() ?: CharacterDetailResponse()
+
+                chImage.setImageResource(MarkerImageUtil.getImage(characterId.toLong()))
+                chTitle.text = characterDetailInfo.name
+                chContent.text = characterDetailInfo.description
+                chLv.text = "Lv." + characterDetailInfo.level
+
+            }
+
+            override fun onFailure(call: Call<CharacterDetailResponse>, t: Throwable) {
+                println(call)
+                println(t)
+                println("====에러=====")
+            }
+
+        })
+
+        close.setOnClickListener(){
+            finish()
+        }
+
+
     }
 }
--- nawanolza/client/app/src/main/java/com/example/nawanolza/CharacterRvAdapter.kt
+++ nawanolza/client/app/src/main/java/com/example/nawanolza/CharacterRvAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.nawanolza
 
 import android.app.Application
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -10,11 +11,13 @@ import com.example.nawanolza.retrofit.CollectionItem
 class CharacterRvAdapter(
     private val characterInfo: List<CollectionItem>,
     private val application: Application,
+    private val intent: Intent,
 )
 :RecyclerView.Adapter<CharacterRvAdapter.MyViewHolder>(){
 
     inner class MyViewHolder(binding: CharacterRvItemBinding):
         RecyclerView.ViewHolder(binding.root) {
+            val binding = binding
             val characterImg = binding.characterImg
             val level = binding.level
         }
@@ -27,11 +30,18 @@ class CharacterRvAdapter(
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val characterInfo = characterInfo[position]
 
-
         if (characterInfo.currentLevel.equals(0))
             holder.characterImg.setImageDrawable(application.getDrawable(MarkerImageUtil.getDarkImage(characterInfo.characterId)))
         else
             holder.characterImg.setImageDrawable(application.getDrawable(MarkerImageUtil.getImage(characterInfo.characterId)))
+
+        holder.characterImg.setOnClickListener {
+            println("===클릭==")
+            println(characterInfo.characterId)
+            intent.putExtra("characterId", characterInfo.characterId.toInt())
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            application.startActivity(intent)
+        }
 
         holder.level.text = "Lv." + characterInfo.currentLevel
     }

d7b706e0cdda2f16994ce1e118761c708510ef61
정동규 <1217jdk@naver.com>
2022-11-18 01:24:09 +0900

    S07P31D103-648 방 삭제

--- nawanolza/client/app/src/main/java/com/example/nawanolza/createGame/Waiting.kt
+++ nawanolza/client/app/src/main/java/com/example/nawanolza/createGame/Waiting.kt
@@ -1,5 +1,6 @@
 package com.example.nawanolza.createGame
 
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -10,15 +11,20 @@ import com.bumptech.glide.Glide
 import com.example.nawanolza.LoginUtil
 import com.example.nawanolza.databinding.ActivityWaitingBinding
 import com.example.nawanolza.retrofit.RetrofitConnection
+import com.example.nawanolza.retrofit.createroom.DeleteRoomResponse
+import com.example.nawanolza.retrofit.createroom.DeleteRoomService
 import com.example.nawanolza.retrofit.enterroom.EnterRoomResponse
 import com.example.nawanolza.retrofit.enterroom.GetRoomResponse
 import com.example.nawanolza.retrofit.enterroom.GetRoomService
+import com.example.nawanolza.stomp.Winner
 import com.example.nawanolza.stomp.waitingstomp.WaitingStompClient
 import com.google.gson.GsonBuilder
+import com.naver.maps.map.overlay.Marker
 import kotlinx.android.synthetic.main.activity_waiting.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import ua.naiksoftware.stomp.Stomp
 
 class Waiting : AppCompatActivity() {
     lateinit var binding: ActivityWaitingBinding
@@ -33,10 +39,14 @@ class Waiting : AppCompatActivity() {
         var tagger: Int = 0
         var runnerList: List<Int> = ArrayList()
         lateinit var entryCode: String
+
+        var _Waiting_Activity: Activity? = null
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _Waiting_Activity = this@Waiting
+
         binding = ActivityWaitingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -74,9 +84,11 @@ class Waiting : AppCompatActivity() {
     }
 
     private fun connectStomp(entryCode: String?) {
+        Log.i("Waiting", "connectStomp onCreate")
         WaitingStompClient.connect()
         WaitingStompClient.subParticipate(entryCode.toString(), adapter, this)
         WaitingStompClient.subGameStart(entryCode.toString(), this@Waiting)
+        WaitingStompClient.subRoomDelete(entryCode.toString(), this@Waiting)
     }
 
     private fun setAdapter() {
@@ -106,5 +118,52 @@ class Waiting : AppCompatActivity() {
                 println(t)
             }
         })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.i("Waiting", "Waiting onDestroy")
+
+        if(hostId == LoginUtil.getMember(this@Waiting)?.id) {
+            val retrofitAPI = RetrofitConnection.getInstance().create(
+                DeleteRoomService::class.java
+            )
+
+            retrofitAPI.deleteRoom(entryCode).enqueue(object:
+                Callback<DeleteRoomResponse> {
+                override fun onResponse(
+                    call: Call<DeleteRoomResponse>,
+                    response: Response<DeleteRoomResponse>
+                ) {
+                    Log.i("DeleteRoom", response.body().toString())
+                }
+
+                override fun onFailure(call: Call<DeleteRoomResponse>, t: Throwable) {
+                    println(call)
+                    println(t)
+                }
+            })
+        }
+
+
+        Log.i("GameEnd", "before memberList size: ${memberList.size}")
+        Log.i("GameEnd", "before memberHash size: ${memberHash.size}")
+        Log.i("GameEnd", "before runnerList size: ${runnerList.size}")
+
+        memberList = ArrayList()
+        memberHash = HashMap()
+        runnerList = ArrayList()
+
+        Log.i("GameEnd", "after memberList size: ${memberList.size}")
+        Log.i("GameEnd", "after memberHash size: ${memberHash.size}")
+        Log.i("GameEnd", "after runnerList size: ${runnerList.size}")
+
+
+        Log.i("GameEnd", "before markerMap size: ${WaitingStompClient.markerMap.size}")
+
+        WaitingStompClient.markerMap = HashMap()
+
+        Log.i("GameEnd", "after markerMap size: ${WaitingStompClient.markerMap.size}")
     }
 }

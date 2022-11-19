65e6e245823631be1557513095cbbcf11eaf0c16
nogusdn <97591002+nogusdn@users.noreply.github.com>
2022-11-19 19:36:34 +0900

    S07P31D103-662 토스트 제거

--- nawanolza/client/app/src/main/java/com/example/nawanolza/LoadingActivity.kt
+++ nawanolza/client/app/src/main/java/com/example/nawanolza/LoadingActivity.kt
@@ -8,7 +8,6 @@ class LoadingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
-        println("토큰 정보 보기 : " + LoginUtil.getAccessToken(this))
 
         val intent : Intent =
             if (LoginUtil.isLogin(this)) Intent(this@LoadingActivity, HomeActivity::class.java)
